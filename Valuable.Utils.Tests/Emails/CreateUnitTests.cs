d00be935621f1067a3bbdee2e4b56e97bbbdfc3a
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-09-12 00:19:37 -0300

    #4 - implementing email

---
+++ Valuable.Utils.Tests/Emails/CreateUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Messages.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Valuables.Utils;
+using Xunit;
+
+namespace Valuable.Utils.Tests.Emails
+{
+    public class CreateUnitTests
+    {
+        [Fact]
+        public void CreateEmail_ValidParameters_ShouldReturnValidResponse()
+        {
+            var text = "any@anything.com";
+
+            var cpfResponse = Email.Create(text);
+
+            cpfResponse.HasError.Should().BeFalse();
+            cpfResponse.Data.Should().NotBeNull();
+            cpfResponse.Data.HasValue.Should().BeTrue();
+            cpfResponse.Data.Value.Should().BeOfType(typeof(Email));
+            cpfResponse.Data.Value.Address.Should().Be(text);
+            cpfResponse.Data.Value.IsValid().Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateEmail_InvalidParameters_ShouldReturnValidResponseWithBusinessError()
+        {
+            var cpfResponse = Email.Create(null);
+
+            cpfResponse.HasError.Should().BeTrue();
+            cpfResponse.Data.HasValue.Should().BeFalse();
+            cpfResponse.Messages.Should().Contain(x => x.Type.Equals(MessageType.BusinessError));
+        }
+    }
+}
---
+++ Valuable.Utils.Tests/Emails/IsValidUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Valuables.Utils;
+using Xunit;
+
+namespace Valuable.Utils.Tests.Emails
+{
+    public class IsValidUnitTests
+    {
+        [Fact]
+        public void ValidateEmail_ValidParameters_ShouldReturnValidResponse()
+        {
+            var text = "none@nothing.com";
+
+            var response = Email.IsValid(text);
+
+            response.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("1813")]
+        [InlineData("@nothing.com")]
+        [InlineData("any.text")]
+        [InlineData("44.35.com@;:\\") ]
+        [InlineData("nothing.com.br")]
+        public void ValidateEmail_InvalidParameters_ShouldReturnValidResponseWithBusinessError(string text)
+        {
+            var response = Email.IsValid(text);
+
+            response.Should().BeFalse();
+        }
+    }
+}
